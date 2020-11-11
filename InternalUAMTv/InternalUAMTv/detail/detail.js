dafe32a6fd64d9098c8639c85816ee22d7aa06b6
Aman srivastava <islam.talapbay@gmail.com>
2020-11-11 15:53:09 +0530

    api done for fav section

--- InternalUAMTv/InternalUAMTv/detail/detail.js
+++ InternalUAMTv/InternalUAMTv/detail/detail.js
@@ -1,6 +1,7 @@
 const URL = "https://api.uam.tv/";
 var details;
 
+
 var init = function () {
        
     initTizenKeys();
@@ -54,26 +55,166 @@ function initTizenKeys()
 
 function getDetailScreenData() {
 	
+
 	
 	document.getElementById("detail_video_name").innerHTML = details["title"];
 	document.getElementById("detail_video_desc").innerHTML = details["desc"];
 	document.getElementById("detail_video_length").innerHTML = formatTime(details["length"]);
 	
+
+	
+	
+
+	var token = localStorage.getItem("jwt token");
+	
+	if(token !== null)
+		{
+			getDirectorList(token);
+		}
+	else
+		{
+			console.log("No token found");
+			location.href = "../login.html";
+		}
+	
+	
+	
+
+	
+}
+
+
+function getDirectorList(token)
+{
+	var castCase = document.getElementById("director_case");
+
+	details["director"].forEach((result, index) => {
+
+
+		let li = document.createElement('li');
+		castCase.appendChild(li);
+
+	    li.innerHTML += result;
+
+    })
+//    
+    	getCastList(token);
+   
+	
+	}
+
+function getCastList(token)
+{
 	var castCase = document.getElementById("cast_case");
 
 	details["cast"].forEach((result, index) => {
 
 
-		var temp = `<p><i class="fa fa-circle text-white mr-3 mt-4"></i> ${result}</p>`;
-		
-		
-		castCase.innerHTML += temp; 
+		let li = document.createElement('li');
+		castCase.appendChild(li);
+
+	    li.innerHTML += result;
 
     })
-	
-	
+ 
+    getFav(token , details["uid"]);
+    
 	
 }
+
+
+
+function setFav(token , uid){
+	
+	
+
+	    let params = {
+	        "mid": uid,
+	    };
+
+	    let query = Object.keys(params)
+	        .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+	        .join('&');
+
+	    fetch(URL + 'v3/users/favourites/post.php?' + query, {
+	    		method: 'POST',
+	            headers: {
+	                'Authorization': "Bearer " + token
+	            },
+	        })
+	        .then(response => response.json())
+	        .then(data => {
+
+	        
+	        	if(data["meta"]["response"] === true)
+	        		{
+	        			getFav(token , details["uid"]);
+	        		}
+
+	       
+
+	        })
+	        .catch((error) => {
+	            console.error('Err:', error);
+	            //hideLoader();
+	        });
+	
+	}
+
+
+function getFav(token , uid){
+	
+	 
+	var isFav = false;
+		 
+		    fetch(URL + 'v3/users/favourites/get.php', {
+		            headers: {
+		                'Authorization': "Bearer " + token
+		            },
+		        })
+		        .then(response => response.json())
+		        .then(data => {
+
+		        	
+		        	data["data"].forEach((result, index) => {
+						
+		        				
+		        		if(result["id_movie"] === uid)
+		        			{
+		        				isFav = true;
+		        				return
+		        			}
+		        		
+		        		
+		        	
+									        
+						})
+						
+							if(isFav === true){
+		        				
+								console.log("yes fav");
+								//set filled heart...
+								
+		        			}
+		        		else{
+		        			
+		        			console.log("no fav");
+		        			
+		        			//set unfilled heart...
+		        		}
+
+		        	
+						
+		        })
+		        .catch((error) => {
+		            console.error('Err:', error);
+		            //hideLoader();
+		        });
+		
+	
+}
+
+
 
 
 function formatTime(totalMinutes){
--- InternalUAMTv/InternalUAMTv/home/home.js
+++ InternalUAMTv/InternalUAMTv/home/home.js
@@ -1,9 +1,12 @@
   
 const URL = "https://api.uam.tv/";
 var categoryList = [];
+var favList = [];
 var mostRecentsList = [];
 var mostViewedList = [];
 var randomBannerList = [];
+
+
 
 var selectedBanner;
 
@@ -15,7 +18,7 @@ var selectedBanner;
 var selectedCatPos = 0,
     selectedViewedPos = 0,
     selectedRecentPos = 0,
-    selectedScreenCatPos = 0; //selected element of any of 3 lists...
+    selectedScreenCatPos = 0; // selected element of any of 3 lists...
 
 
 var init = function() {
@@ -26,7 +29,7 @@ var init = function() {
 
     viewLoader();
 
-    //set Focus on details text....
+    // set Focus on details text....
 
     setFocus("watch_btn_id", "watch_btn");
     setFocus("watch_btn_id", "onLeft");
@@ -59,24 +62,24 @@ function initTizenKeys() {
     // add eventListener for keydown
     document.addEventListener('keydown', function(e) {
         switch (e.keyCode) {
-            case 37: //LEFT arrow
+            case 37: // LEFT arrow
                 moveLeft();
                 break;
-            case 38: //UP arrow
+            case 38: // UP arrow
                 moveUp();
                 break;
-            case 39: //RIGHT arrow
+            case 39: // RIGHT arrow
                 moveRight();
                 break;
-            case 40: //DOWN arrow
+            case 40: // DOWN arrow
                 moveDown();
                 break;
-            case 13: //OK button
+            case 13: // OK button
                 moveOk();
 
 
                 break;
-            case 10009: //RETURN button
+            case 10009: // RETURN button
                 tizen.application.getCurrentApplication().exit();
                 break;
             default:
@@ -137,7 +140,7 @@ function moveOk() {
 
 function moveUp() {
 
-    //on left not null ie. control is on side nav bar...
+    // on left not null ie. control is on side nav bar...
     if (document.getElementsByClassName("onLeft")[0] !== undefined) {
 
         var el = document.getElementsByClassName("watch_btn")[0].id;
@@ -161,7 +164,7 @@ function moveUp() {
     }
 
 
-    //start else..
+    // start else..
     else {
 
         if (document.getElementsByClassName("activeCategory")[0] !== undefined) {
@@ -185,7 +188,7 @@ function moveUp() {
             removeFocus("button_favourite");
         } else if (document.getElementsByClassName("activeViewed")[0] !== undefined) {
             scroll('-=500px');
-            //		   selectedListPos = 0 ;
+            // selectedListPos = 0 ;
             setFocus("categories " + selectedCatPos, "activeCategory");
 
             removeFocus("activeViewed");
@@ -196,7 +199,7 @@ function moveUp() {
         } else if (document.getElementsByClassName("activeRecents")[0] !== undefined) {
 
             scroll('-=200px');
-            //	selectedListPos = 0 ;
+            // selectedListPos = 0 ;
             setFocus("viewed " + selectedViewedPos, "activeViewed");
 
 
@@ -209,14 +212,14 @@ function moveUp() {
 
     }
 
-    //end else..	
+    // end else..
 
 }
 
 
 function moveDown() {
 
-    //on left not null ie. control is on side nav bar...
+    // on left not null ie. control is on side nav bar...
     if (document.getElementsByClassName("onLeft")[0] !== undefined) {
 
         var el = document.getElementsByClassName("watch_btn")[0].id;
@@ -249,7 +252,7 @@ function moveDown() {
             removeFocus("activeDetail");
 
         } else if (document.getElementsByClassName("button_play")[0] !== undefined) {
-            //  selectedListPos = 0 ;
+            // selectedListPos = 0 ;
             setFocus("categories " + selectedCatPos, "activeCategory");
             setFocus("cat-wrap " + selectedCatPos, "cardhover");
 
@@ -279,7 +282,7 @@ function moveDown() {
         } else if (document.getElementsByClassName("activeViewed")[0] !== undefined) {
 
             scroll('+=300px');
-            //	selectedListPos = 0 ;
+            // selectedListPos = 0 ;
 
             removeFocus("activeViewed");
             removeFocus("cardhover");
@@ -389,7 +392,7 @@ function moveLeft() {
     }
 
 
-    //	activeScreenCategory
+    // activeScreenCategory
     else if (document.getElementsByClassName("activeScreenCategory")[0] !== undefined) {
         if (selectedScreenCatPos !== 0) {
 
@@ -412,7 +415,7 @@ function moveLeft() {
 
 
 
-    //move to side navigation drawer
+    // move to side navigation drawer
     else if (document.getElementsByClassName("activeDetail")[0] !== undefined) {
         removeFocus("activeDetail");
         var onLeftElementId = document.getElementsByClassName("watch_btn")[0].id;
@@ -446,7 +449,7 @@ function scroll(by) {
 
 
 function moveRight() {
-    //on left not null ie. control is on side nav bar...
+    // on left not null ie. control is on side nav bar...
     if (document.getElementsByClassName("onLeft")[0] !== undefined) {
 
         removeFocus("onLeft");
@@ -456,11 +459,15 @@ function moveRight() {
         } else if (onLeftElementId === "category_btn_id") {
 
             setFocus("categoryScreen " + selectedScreenCatPos, "activeScreenCategory");
+            
+            //my focus.....
+            
+            //setFocus("categories-wrap " + selectedScreenCatPos, "cardhover");
         }
 
     }
 
-    //start else
+    // start else
     else {
 
         if (document.getElementsByClassName("button_play")[0] !== undefined) {
@@ -562,7 +569,7 @@ function moveRight() {
             }
         }
 
-        //end else
+        // end else
     }
 
 
@@ -629,7 +636,7 @@ function getRandomMovies(token) {
             data.forEach((result, index) => {
 
 
-                //add categories to list.....
+                // add categories to list.....
                 var obj = {
                     "fullId": result["src"]["id_full"],
                     "year": result["meta"]["year"],
@@ -640,7 +647,10 @@ function getRandomMovies(token) {
                     "image": "https://media.uam.tv/images/media/frames/" + result["src"]["id_full"] + ".jpg",
                     "desc": result["langs"]["it"]["logline"],
                     "tag": result["langs"]["it"]["tags"],
-                    "geolimits": result["meta"]["geolimits"]
+                    "geolimits": result["meta"]["geolimits"],
+                    "trailerId": result["src"]["id_trailer"],
+                    "uid": result["uid"]
+               
                 };
 
                 randomBannerList.push(obj);
@@ -653,10 +663,50 @@ function getRandomMovies(token) {
         })
         .catch((error) => {
             console.error('Err:', error);
-            //		  hideLoader();
+            // hideLoader();
         });
 }
 
+
+function getFavourites(token){
+	
+				    fetch(URL + 'v3/users/favourites/get.php', {
+			            headers: {
+			                'Authorization': "Bearer " + token
+			            },
+			        })
+			        .then(response => response.json())
+			        .then(data => {
+
+			        	 data["data"].forEach((result, index) => {
+
+
+			                
+			                     // add favorites to list.....
+			                     var obj = {
+			                         "fullId" : result["id_full"],
+			                          "title" : result["title"],
+			                         "image": "https://media.uam.tv/images/media/slider/" + result["id_full"] + ".jpg"
+			                     };
+
+			                     favList.push(obj);
+			           
+
+			             })
+
+
+			             getMostRecents(token);
+
+			        	
+							
+			        })
+			        .catch((error) => {
+			            console.error('Err:', error);
+			            // hideLoader();
+			        });
+	
+	
+}
 
 function getCategories(token) {
     fetch(URL + 'v3/movies/categories/get.php', {
@@ -671,7 +721,7 @@ function getCategories(token) {
 
 
                 if (result[0] !== -1 && result[0] !== 0) {
-                    //add categories to list.....
+                    // add categories to list.....
                     var obj = {
                         "fullId": result[0],
                         "title": result[1],
@@ -685,13 +735,13 @@ function getCategories(token) {
 
 
 
-            addCategories();
-            getMostRecents(token);
+            addCategories(token);
+            
 
         })
         .catch((error) => {
             console.error('Err:', error);
-            //		  hideLoader();
+            // hideLoader();
         });
 }
 
@@ -707,7 +757,7 @@ function getMostRecents(token) {
 
             data.forEach((result, index) => {
 
-                //add most recents to list.....
+                // add most recents to list.....
                 var obj = {
                     "fullId": result["src"]["id_full"],
                     "image": "https://media.uam.tv/images/media/slider/" + result["src"]["id_full"] + ".jpg"
@@ -723,7 +773,7 @@ function getMostRecents(token) {
         })
         .catch((error) => {
             console.error('Err:', error);
-            //		  hideLoader();
+            // hideLoader();
         });
 }
 
@@ -738,7 +788,7 @@ function getMostViewed(token) {
 
             data["data"].forEach((result, index) => {
 
-                //add most viewed to list.....
+                // add most viewed to list.....
 
                 var obj = {
                     "fullId": result["id_full"],
@@ -754,7 +804,7 @@ function getMostViewed(token) {
         })
         .catch((error) => {
             console.error('Err:', error);
-            //		  hideLoader();
+            // hideLoader();
         });
 }
 
@@ -802,7 +852,7 @@ function addCategoriesToCategoryScreen() {
         var row = document.getElementById("row-category " + rowId);
         var temp = `
         <div id="categoryScreen ${idx}" class="col-lg-3 mt-4">
-        <div class="card" style="width: 18rem;">
+        <div id="categories-wrap ${idx}" class="card" style="width: 18rem;">
             <img class="card-img-top" src="${result["image"]}" alt="Card image cap">
             <p class="category_type">${result["title"]}</p>
         </div>
@@ -821,7 +871,7 @@ function addCategoriesToCategoryScreen() {
 
 }
 
-function addCategories() {
+function addCategories(token) {
 
     var rowId = "-1";
 
@@ -868,6 +918,10 @@ function addCategories() {
 
 
     })
+    
+    
+    getFavourites(token);
+    
 }
 
 
@@ -995,7 +1049,7 @@ function addFavouritesToFavouritesScreen() {
     var rowId = "-1";
 
 
-    categoryList.forEach((result, idx) => {
+    favList.forEach((result, idx) => {
 
 
         console.log(idx);
@@ -1022,8 +1076,8 @@ function addFavouritesToFavouritesScreen() {
 
         var row = document.getElementById("row-favourite " + rowId);
         var temp = `      
-        <div id="viewedScreen ${idx}" class="col-lg-3 mt-4">
-        <div class="card" style="width: 18rem;">
+        <div id="favoriteScreen ${idx}" class="col-lg-3 mt-4">
+        <div id="fav-wrap ${idx}" class="card" style="width: 18rem;">
             <img class="card-img-top" src="${result["image"]}" alt="Card image cap">
             <p class="category_type">${result["title"]}</p>
         </div>
@@ -1034,7 +1088,7 @@ function addFavouritesToFavouritesScreen() {
         row.innerHTML += temp;
     })
 
-    //scroll to top of home...
+    // scroll to top of home...
 
     removeFocusHome();
 
@@ -1080,7 +1134,7 @@ function changeBg(image) {
 
 
 
-function setMovie() { //check geolimit
+function setMovie() { // check geolimit
 
     viewLoader();
 
@@ -1112,7 +1166,7 @@ function setMovie() { //check geolimit
 
 }
 
-function getMovieSource(moviePlay, token) { //hit stream api...
+function getMovieSource(moviePlay, token) { // hit stream api...
 
     let params = {
         "contentID": moviePlay["contentId"],
@@ -1131,7 +1185,7 @@ function getMovieSource(moviePlay, token) { //hit stream api...
         .then(response => response.json())
         .then(data => {
 
-            //set to storage
+            // set to storage
 
             var videoUrl = data["data"]["embedUrlList"][0]["https"]["abr"]["hls"];
 
@@ -1170,7 +1224,7 @@ function removeFocusHome() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 
-    //Assign focus to detail button....
+    // Assign focus to detail button....
     if (document.getElementsByClassName("activeDetail")[0] !== undefined) {
         remove("activeDetail");
     }
